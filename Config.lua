178653bc523636166e56e2437f5bbfcac78ce187
噶玛噶举 <81478693+extkarma@users.noreply.github.com>
2021-03-27 16:33:50 +0100

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,11 @@
+Config = {}
+Config.Buttons = {
+    {index = 0,name = 'Discord',url = 'https://discord.gg/TbxqKfQe'},
+    {index = 1,name = 'Se connecter au serveur',url = 'fivem://connect/176.9.67.34:30988'}
+}
+
+
+
+--------------------------------
+------- by 噶玛噶举#2649 -------
+--------------------------------
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author '噶玛噶举#2649'
+
+description 'button discord status . Serveur de dev https://discord.gg/Ch329cc2Q5'
+
+shared_script 'Config.lua'
+client_script 'Client.lua'
